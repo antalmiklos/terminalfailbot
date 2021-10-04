8cac066fc5dde54db18ba35453f4f4842d6dc18f
Miki <antalmiklos4@gmail.com>
2021-10-04 22:05:31 +0200

    client stuff

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 verbose_mode = True
 @app.route('/submit', methods=['POST'])
 def submit_message():
-    content = json.load(request.get_json())
+    content = json.loads(request.get_json())
     print(content)
 
     tweet = Tweet(content['message'], content['author'], config)
